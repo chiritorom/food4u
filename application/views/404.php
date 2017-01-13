7dee87c8641894d8ee4b16dfa75c3e977d88f382
PC-SHEYLA <sheylachirito@gmail.com>
2017-01-13 15:30:40 -0500

    404

--- application/views/404.php
+++ application/views/404.php
@@ -1,1 +1,22 @@
-404.php
+<html>
+	
+	<head>
+		<meta charset="utf-8">
+		<title>404 Not Found</title>
+
+		<link rel="shortcut icon" href="favicon.ico">
+		<link rel="stylesheet" type="text/css" href="<?= base_url() ?>/assets/css/style.css">
+	</head>
+
+	<body>	
+
+		<div id="texto">
+		<h1>ERROR 404</h1>
+		<h2>¡Oh, no! </br> Esta página no existe.</h2>
+
+		<button type="button">FOOD4U</button>
+		</div>
+
+	</body>
+
+</html>
