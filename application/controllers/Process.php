623d7ce34cb0c4d3019bb1bdec3f67f935efae73
CARLOS-PC <chiritorom@gmail.com>
2017-01-12 23:11:50 -0500

    BOTON EDITAR, ELIMINAR FUNCIONANDO EN PLATOS

--- application/controllers/Process.php
+++ application/controllers/Process.php
@@ -7,14 +7,15 @@ class Process extends CI_Controller {
         parent::__construct();
         $this->load->model("UserAdmin");
         $this->load->model("Plate");
+        
     }
 	
 	public function platos() {
     
     if($this->input->post("id")):
-    $idPage = $this->input->post("id");
+    $idPlate = $this->input->post("id");
     
-    $result = $this->Plate->findById($idPage);
+    $result = $this->Plate->findById($idPlate);
     
     echo '<div class="form-plate">
             <div class="button-close">
@@ -25,9 +26,7 @@ class Process extends CI_Controller {
               <div class="img-plate">
                 <img src="' . base_url() . 'assets/images/this-week01.jpg" alt="">
                 <div class="file">
-                  <p>
-                    Elegir archivo
-                  </p>
+                  <p>Elegir archivo</p>
 
                   <input type="file">
                 </div>
@@ -59,13 +58,12 @@ class Process extends CI_Controller {
 	
 	public function nuevo_plato() {
     
-    
     echo '<div class="form-plate">
             <div class="button-close">
               <a href=""><i class="fa fa-close" aria-hidden="true"></i></a>
             </div>
 
-            <form action="">
+            <form action="' . base_url() . 'process/agregar_nuevo_plato" method="POST" enctype="multipart/form-data">
               <div class="img-plate">
                 <img src="' . base_url() . 'assets/images/this-week01.jpg" alt="">
                 <div class="file">
@@ -73,17 +71,17 @@ class Process extends CI_Controller {
                     Elegir archivo
                   </p>
 
-                  <input type="file">
+                  <input type="file" name="imagen">
                 </div>
               </div>
 
               <div class="content-form">
 								<label for="">NOMBRE</label>
-                <input type="text" name="nombre">
+                <input type="text" name="nombre" required>
                 <label for="">PRECIO</label>
-                <input type="text" name="precio">
+                <input type="text" name="precio" required>
                 <label for="">DESCRIPCIÓN</label>
-                <textarea name="descripcion"></textarea>
+                <textarea name="descripcion" required></textarea>
                 
                 <button type="submit">Agregar</button>
                 <button type="text">Cancelar</button> 
@@ -91,5 +89,38 @@ class Process extends CI_Controller {
             </form>
           </div>';
 	}
+
+  public function agregar_nuevo_plato() {
+    $nombre = $this->input->post("nombre");
+    $precio = $this->input->post("precio");
+    $descripcion = $this->input->post("descripcion");
+   // $imagen = $this->input->post("imagen");
+
+    $config['upload_path'] = './assets/images';
+    $config['allowed_types'] = 'jpg|png';
+    $config['max_size'] = '100';
+    $config['max_width']  = '1024';
+    $config['max_height']  = '768';
+
+
+    $this->load->library('upload', $config);
+
+    if ( ! $this->upload->do_upload("imagen")) {
+      $error = array('error' => $this->upload->display_errors());
+
+    } else {
+      $data = array('upload_data' => $this->upload->data());
+    }
+
+    $imagen = $this->upload->data("file_name");
+
+    $this->Plate->addPlate($nombre, $precio, $descripcion, $imagen);
+  }
+
+  public function eliminar_plato() {
+    $idPlate = $this->input->post("id");
+    
+    $this->Plate->deletePlate($idPlate);
+  }
   
 }
--- assets/js/ncode.js
+++ assets/js/ncode.js
@@ -47,6 +47,27 @@ $(".add-plate button").on("click", function() {
 		success: function(resp) {
 			$("#modal-plate").html(resp);
 				$("#modal-plate").toggle();
+
+			$('.img-plate input').change(function(e) {
+		      addImage(e); 
+		     });
+
+		     function addImage(e){
+		      var file = e.target.files[0],
+		      imageType = /image.*/;
+		    
+		      if (!file.type.match(imageType))
+		       return;
+		  
+		      var reader = new FileReader();
+		      reader.onload = fileOnload;
+		      reader.readAsDataURL(file);
+		     }
+		  
+		     function fileOnload(e) {
+		      var result=e.target.result;
+		      $('.img-plate img').attr("src",result);
+		     }
 			
 			$("#modal-plate .form-plate .button-close a, #modal-plate .form-plate .content-form button:last-child").on("click", function(e) {
 				e.preventDefault();
@@ -55,6 +76,27 @@ $(".add-plate button").on("click", function() {
 		}
 	});
 });
+
+$(".table-plate .group-delete a:first-child").on("click", function(e) {
+	e.preventDefault();
+
+	var $me = $(this);
+	var $id = $me.attr("data-id");
+
+	$.ajax({
+		url: $me.attr("data-action"),
+		method: "post",
+		data: {
+			id: $id
+		},
+		success: function(resp) {
+			alert("eliminado");
+			location.reload();
+		}
+	});
+});
+
+
 
 
 
